59854d211991234eeda8745721c61b823f740af5
Nikhil79004 <mehranikhilsingh@gmail.com>
2025-01-05 20:27:16 +0530

    changes in product and navbar file

--- src/Home/Product.jsx
+++ src/Home/Product.jsx
@@ -8,7 +8,7 @@ function Product() {
     <div className='grid grid-cols-1 p-2 mt-10 mb-10 md:grid-cols-2 lg:grid-cols-2'>
       {[...Array(2)].map((_,index)=>(
       
-      <div key={index} className='flex flex-col items-center justify-center p-10 mx-auto border border-blue-400 rounded-lg h-72 w-60 bg-gradient-to-r from-orange-200 via-white to-green-200'>
+      <div key={index} className='flex flex-col items-center justify-center p-10 mx-auto border border-blue-400 rounded-lg h-72 w-60 bg-gradient-to-r from-white to-zinc-400'>
       <p>Keep love in your heart. A life without it is like a sunless garden when the flowers are dead. The consciousness of loving and being loved brings a warmth and a richness to life that nothing else can bring.</p>
       </div>
   ))}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         <a href="#">Explore</a>
       </li>
     </>
-  );
+    );
 
   return (
     <div>
